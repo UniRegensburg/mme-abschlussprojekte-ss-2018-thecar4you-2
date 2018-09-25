871285b6ed6e46de5348b0e598757b1c69862cc0
MonikaRi <megamindroxanne@mail.ru>
2018-09-25 14:17:04 +0200

    Beschreibung zu Wizardschritten angepasst, Titel zu Bildern und Span-Elementen hinzugefügt, kleinere Styleanpassungen

--- js/CarView.js
+++ js/CarView.js
@@ -39,7 +39,7 @@ CarApp.CarView = function() {
   }
 
   //Die Funktion erzeugt dynamisch Spans
-  function spanGenerator(parentTag, spanClass, classIndex, htmlText){
+  function spanGenerator(parentTag, spanClass, classIndex, htmlText, spanTitle){
     let newSpan = document.createElement("span"),
     getParent = document.getElementsByClassName(parentTag)[classIndex].appendChild(newSpan);
     newSpan.className = spanClass;
@@ -47,10 +47,11 @@ CarApp.CarView = function() {
       newSpan.draggable = "true";
     }
     getParent.innerHTML = htmlText;
+	getParent.title = spanTitle;
   }
 
   //Die Funktion erzeugt dynamisch Images
-  function imageGenerator(imgSource, imgId, imgClass, classIndex){
+  function imageGenerator(imgSource, imgId, imgClass, classIndex, imgTitle){
     let newImg = document.createElement("img"),
     getParent = document.getElementsByClassName(imgClass)[classIndex];
     newImg.src = imgSource;
@@ -59,99 +60,100 @@ CarApp.CarView = function() {
       newImg.draggable = "true";
         }
     getParent.appendChild(newImg);
+	newImg.title = imgTitle;
   }
 
 /*startscreen**************************************************************************************** */
 
   function setStartPictures(){
-    imageGenerator("Pictures/StartImg1.png","startImg","picturesStart",0);
+    imageGenerator("Pictures/StartImg1.png","startImg","picturesStart",0,"");
   }
 
 /*preis**************************************************************************************** */
 
   function setPricePictures(){
-    imageGenerator("Pictures/Money500.png","money500","lowMoney", 0);
-    imageGenerator("Pictures/Money1000.png","money1000","lowMoney", 0);
-    imageGenerator("Pictures/Money5000.png","money5000","lowMoney", 0);
-    imageGenerator("Pictures/Money10000.png","money10000","highMoney", 0);
-    imageGenerator("Pictures/Money50000.png","money50000","highMoney", 0);
-    imageGenerator("Pictures/Money100000.png","money100000","highMoney", 0);
-    imageGenerator("Pictures/Sparschwein2.png", "piggybank", "bank", 0);
+    imageGenerator("Pictures/Money500.png","money500","lowMoney", 0, "");
+    imageGenerator("Pictures/Money1000.png","money1000","lowMoney", 0, "");
+    imageGenerator("Pictures/Money5000.png","money5000","lowMoney", 0, "");
+    imageGenerator("Pictures/Money10000.png","money10000","highMoney", 0, "");
+    imageGenerator("Pictures/Money50000.png","money50000","highMoney", 0, "");
+    imageGenerator("Pictures/Money100000.png","money100000","highMoney", 0, "");
+    imageGenerator("Pictures/Sparschwein2.png", "piggybank", "bank", 0, "");
   }
 
   function setPriceUserActions(){
-    spanGenerator("user-action-price", "material-icons replay-button", 0, "replay");
-    spanGenerator("user-action-price", "material-icons delete-button", 0, "delete");
-    spanGenerator("user-action-price", "budget", 0, "0");
+    spanGenerator("user-action-price", "material-icons replay-button", 0, "replay", "Rückgängig");
+    spanGenerator("user-action-price", "material-icons delete-button", 0, "delete", "Löschen");
+    spanGenerator("user-action-price", "budget", 0, "0", "");
   }
 
 /*alter************************************************************************************** */
 
   function setAlterPictures() {
-    imageGenerator("Pictures/Plattenspieler.png", "platte", "picturesAlter", 0);
-    imageGenerator("Pictures/Tape1.png", "tape", "picturesAlter", 0);
-    imageGenerator("Pictures/CD-Rom.png", "cd", "picturesAlter", 0);
-    imageGenerator("Pictures/mp3Player1.png", "mp3", "picturesAlter", 0);
-    imageGenerator("Pictures/Smartspeaker.png", "smartspeaker", "picturesAlter", 0);
+    imageGenerator("Pictures/Plattenspieler.png", "platte", "picturesAlter", 0, "Schallplatte");
+    imageGenerator("Pictures/Tape1.png", "tape", "picturesAlter", 0, "Kasette");
+    imageGenerator("Pictures/CD-Rom.png", "cd", "picturesAlter", 0, "CD-Rom");
+    imageGenerator("Pictures/mp3Player1.png", "mp3", "picturesAlter", 0, "MP3-Player");
+    imageGenerator("Pictures/Smartspeaker.png", "smartspeaker", "picturesAlter", 0, "Smartspeaker");
   }
 
 /*km**************************************************************************************** */
 
   function setKMPictures() {
-    imageGenerator("Pictures/PlanetMars1.png", "mars", "picturesKM", 0);
-    imageGenerator("Pictures/PlanetErde3.png", "erde", "picturesKM", 0);
-    imageGenerator("Pictures/MoonPhobos.png", "phobos", "picturesKM", 0);
-    imageGenerator("Pictures/PlanetNeptun.png", "neptun", "picturesKM", 0);
+    imageGenerator("Pictures/PlanetMars1.png", "mars", "picturesKM", 0, "Mars");
+    imageGenerator("Pictures/PlanetErde3.png", "erde", "picturesKM", 0, "Erde");
+    imageGenerator("Pictures/MoonPhobos.png", "phobos", "picturesKM", 0, "Phobos");
+    imageGenerator("Pictures/PlanetNeptun.png", "neptun", "picturesKM", 0, "Neptun");
   }
 
 /*strecke**************************************************************************************** */
 
   function setDistanceUserActions(){
-    imageGenerator("Pictures/workG.png", "work-buttonT", "work-strecke", 0);
-    imageGenerator("Pictures/workY.png", "work-buttonO", "work-strecke", 0);
-    imageGenerator("Pictures/workR.png", "work-buttonS", "work-strecke", 0);
-    imageGenerator("Pictures/shoppingG.png", "shopping-buttonT", "shopping-strecke", 0);
-    imageGenerator("Pictures/shoppingY.png", "shopping-buttonO", "shopping-strecke", 0);
-    imageGenerator("Pictures/shoppingR.png", "shopping-buttonS", "shopping-strecke", 0);
-    imageGenerator("Pictures/schoolG.png", "school-buttonT", "school-strecke", 0);
-    imageGenerator("Pictures/schoolY.png", "school-buttonO", "school-strecke", 0);
-    imageGenerator("Pictures/schoolR.png", "school-buttonS", "school-strecke", 0);
-    imageGenerator("Pictures/hobbyG.png", "hobby-buttonT", "hobby-strecke", 0);
-    imageGenerator("Pictures/hobbyY.png", "hobby-buttonO", "hobby-strecke", 0);
-    imageGenerator("Pictures/hobbyR.png", "hobby-buttonS", "hobby-strecke", 0);
+    imageGenerator("Pictures/workG.png", "work-buttonT", "work-strecke", 0, "Arbeit (Täglich)");
+    imageGenerator("Pictures/workY.png", "work-buttonO", "work-strecke", 0, "Arbeit (Oft)");
+    imageGenerator("Pictures/workR.png", "work-buttonS", "work-strecke", 0, "Arbeit (Selten)");
+    imageGenerator("Pictures/shoppingG.png", "shopping-buttonT", "shopping-strecke", 0, "Einkaufen (Täglich)");
+    imageGenerator("Pictures/shoppingY.png", "shopping-buttonO", "shopping-strecke", 0, "Einkaufen (Oft)");
+    imageGenerator("Pictures/shoppingR.png", "shopping-buttonS", "shopping-strecke", 0, "Einkaufen (Selten)");
+    imageGenerator("Pictures/schoolG.png", "school-buttonT", "school-strecke", 0, "Bildung (Täglich)");
+    imageGenerator("Pictures/schoolY.png", "school-buttonO", "school-strecke", 0, "Bildung (Oft)");
+    imageGenerator("Pictures/schoolR.png", "school-buttonS", "school-strecke", 0, "Bildung (Selten)");
+    imageGenerator("Pictures/hobbyG.png", "hobby-buttonT", "hobby-strecke", 0, "Freizeit (Täglich)");
+    imageGenerator("Pictures/hobbyY.png", "hobby-buttonO", "hobby-strecke", 0, "Freizeit (Oft)");
+    imageGenerator("Pictures/hobbyR.png", "hobby-buttonS", "hobby-strecke", 0, "Freizeit (Selten)");
   }
 
 /*ps*************************************************************************************** */
 
   function setPsPictures() {
-    imageGenerator("Pictures/AutoCorsa.jpg", "corsa", "picturesPs", 0);
-    imageGenerator("Pictures/AutoBMW3er.jpg", "bmw", "picturesPs", 0);
-    imageGenerator("Pictures/AutoTruck.jpg", "truck", "picturesPs", 0);
+    imageGenerator("Pictures/AutoCorsa.jpg", "corsa", "picturesPs", 0, "");
+    imageGenerator("Pictures/AutoBMW3er.jpg", "bmw", "picturesPs", 0, "");
+    imageGenerator("Pictures/AutoTruck.jpg", "truck", "picturesPs", 0, "");
   }
 
 /*sitze********************************************************************************** */
 
   function setSeatPictures() {
-	imageGenerator("Pictures/SitzplätzeZweier.png", "zweierSitz", "zwei-sitzer", 0);
-	imageGenerator("Pictures/SitzplätzeFünfer.png", "fünferSitz", "fünf-sitzer", 0);
-	imageGenerator("Pictures/SitzplätzeSiebener.png", "siebenerSitz", "sieben-sitzer", 0);
+	imageGenerator("Pictures/SitzplätzeZweier.png", "zweierSitz", "zwei-sitzer", 0, "");
+	imageGenerator("Pictures/SitzplätzeFünfer.png", "fünferSitz", "fünf-sitzer", 0, "");
+	imageGenerator("Pictures/SitzplätzeSiebener.png", "siebenerSitz", "sieben-sitzer", 0, "");
   }
 
   function setSeatUserActions() {
     for(let i = 0; i < 2; i++){
-      spanGenerator("ZweiSitz-icons", "material-icons two-seats-span", 0, "crop_square");
-      spanGenerator("FünfSitz-front-icons", "material-icons five-seats-span-front", 0, "crop_square");
-      spanGenerator("SiebenSitz-front-icons", "material-icons seven-seats-span-front", 0, "crop_square");
-      spanGenerator("SiebenSitz-middle-icons", "material-icons seven-seats-span-middle", 0, "crop_square");
+      spanGenerator("ZweiSitz-icons", "material-icons two-seats-span", 0, "crop_square", "");
+      spanGenerator("FünfSitz-front-icons", "material-icons five-seats-span-front", 0, "crop_square", "");
+      spanGenerator("SiebenSitz-front-icons", "material-icons seven-seats-span-front", 0, "crop_square", "");
+      spanGenerator("SiebenSitz-middle-icons", "material-icons seven-seats-span-middle", 0, "crop_square", "");
     }
 
     for(let i = 0; i < 3; i++){
-      spanGenerator("FünfSitz-back-icons", "material-icons five-seats-span-back", 0, "crop_square");
-      spanGenerator("SiebenSitz-back-icons", "material-icons seven-seats-span-back", 0, "crop_square");
+      spanGenerator("FünfSitz-back-icons", "material-icons five-seats-span-back", 0, "crop_square", "");
+      spanGenerator("SiebenSitz-back-icons", "material-icons seven-seats-span-back", 0, "crop_square", "");
     }
-  spanGenerator("person-icons", "material-icons delete_forever-button", 0 ,"seat_delete");
-	spanGenerator("person-icons", "material-icons parent-icon", 0, "person");
-	spanGenerator("person-icons", "material-icons child-icon", 0, "child_care");
+	spanGenerator("person-icons", "material-icons delete_forever-button", 0 ,"seat_delete", "Löschen");
+	spanGenerator("person-icons", "material-icons parent-icon", 0, "person", "Erwachsener");
+	spanGenerator("person-icons", "material-icons child-icon", 0, "child_care", "Kind");
   }
 
   function updateSeatPic(seat, type) {
@@ -175,13 +177,13 @@ CarApp.CarView = function() {
 /*verbrauch********************************************************************************** */
 
   function setVerbrauchPictures() {
-    imageGenerator("Pictures/Tropfen2.png", "tropfen", "picturesVerbrauch", 0);
+    imageGenerator("Pictures/Tropfen2.png", "tropfen", "picturesVerbrauch", 0, "");
   }
 
   function setVerbrauchUserActions(){
-    spanGenerator("user-action-verbrauch", "material-icons remove-button", 0, "remove");
-    spanGenerator("user-action-verbrauch", "material-icons add-button", 0, "add");
-    spanGenerator("user-action-verbrauch", "verbrauch", 0, "10L/100Km");
+    spanGenerator("user-action-verbrauch", "material-icons remove-button", 0, "remove", "");
+    spanGenerator("user-action-verbrauch", "material-icons add-button", 0, "add", "");
+    spanGenerator("user-action-verbrauch", "verbrauch", 0, "10L/100Km", "");
   }
 
   function adjustDrop() {
@@ -196,8 +198,8 @@ CarApp.CarView = function() {
 /*kraftstoff********************************************************************************** */
 
   function setKraftstoffPictures() {
-	spanGenerator("benzin-action", "material-icons benzinPic", 0, "local_gas_station");
-	spanGenerator("diesel-action", "material-icons dieselPic", 0, "local_gas_station");
+	spanGenerator("benzin-action", "material-icons benzinPic", 0, "local_gas_station", "");
+	spanGenerator("diesel-action", "material-icons dieselPic", 0, "local_gas_station", "");
   }
 
 /*erg*************************************************************************************** */
